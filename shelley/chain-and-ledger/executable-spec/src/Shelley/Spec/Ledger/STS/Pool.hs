28e2a9fe0039f10174ae2ef5e106500753e8eb41
Jared Corduan <jared.corduan@iohk.io>
2020-05-18 13:42:45 -0400

    handle late registering stake pools

--- shelley/chain-and-ledger/executable-spec/src/Shelley/Spec/Ledger/STS/Pool.hs
+++ shelley/chain-and-ledger/executable-spec/src/Shelley/Spec/Ledger/STS/Pool.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE EmptyDataDecls #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -13,24 +14,23 @@ module Shelley.Spec.Ledger.STS.Pool
 where
 
 import Byron.Spec.Ledger.Core (dom, (∈), (∉), (⋪))
-import Cardano.Binary (FromCBOR (..), ToCBOR (..), decodeWord)
+import Cardano.Binary (FromCBOR (..), ToCBOR (..), decodeListLen, decodeWord, encodeListLen, matchSize)
 import Cardano.Prelude (NoUnexpectedThunks (..))
 import Control.Monad.Trans.Reader (asks)
-import Control.State.Transition
+import Control.State.Transition ((?!), STS (..), TRC (..), TransitionRule, failBecause, judgmentContext, liftSTS)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 import Data.Typeable (Typeable)
 import Data.Word (Word8)
 import GHC.Generics (Generic)
-import Shelley.Spec.Ledger.BaseTypes
-import Shelley.Spec.Ledger.Crypto
-import Shelley.Spec.Ledger.Delegation.Certificates
-import Shelley.Spec.Ledger.Keys
-import Shelley.Spec.Ledger.LedgerState
-import Shelley.Spec.Ledger.PParams
-import Shelley.Spec.Ledger.Slot
-import Shelley.Spec.Ledger.TxData
+import Shelley.Spec.Ledger.BaseTypes (Globals (..), ShelleyBase, invalidKey)
+import Shelley.Spec.Ledger.Crypto (Crypto)
+import Shelley.Spec.Ledger.Keys (KeyHash (..), KeyRole (..))
+import Shelley.Spec.Ledger.LedgerState (PState (..), emptyPState)
+import Shelley.Spec.Ledger.PParams (PParams, PParams' (..))
+import Shelley.Spec.Ledger.Slot ((*-), Duration (..), EpochNo (..), SlotNo, epochInfoEpoch, epochInfoFirst)
+import Shelley.Spec.Ledger.TxData (DCert (..), PoolCert (..), PoolParams (..), StakePools (..))
 
 data POOL crypto
 
@@ -48,8 +48,8 @@ instance STS (POOL crypto) where
   type BaseM (POOL crypto) = ShelleyBase
 
   data PredicateFailure (POOL crypto)
-    = StakePoolNotRegisteredOnKeyPOOL
-    | StakePoolRetirementWrongEpochPOOL
+    = StakePoolNotRegisteredOnKeyPOOL (KeyHash 'StakePool crypto)
+    | StakePoolRetirementWrongEpochPOOL EpochNo EpochNo EpochNo
     | WrongCertificateTypePOOL
     deriving (Show, Eq, Generic)
 
@@ -64,19 +64,33 @@ instance
   ToCBOR (PredicateFailure (POOL crypto))
   where
   toCBOR = \case
-    StakePoolNotRegisteredOnKeyPOOL -> toCBOR (0 :: Word8)
-    StakePoolRetirementWrongEpochPOOL -> toCBOR (1 :: Word8)
-    WrongCertificateTypePOOL -> toCBOR (2 :: Word8)
+    StakePoolNotRegisteredOnKeyPOOL hk ->
+      encodeListLen 2 <> toCBOR (0 :: Word8) <> toCBOR hk
+    StakePoolRetirementWrongEpochPOOL current e eMax ->
+      encodeListLen 4 <> toCBOR (1 :: Word8) <> toCBOR current <> toCBOR e <> toCBOR eMax
+    WrongCertificateTypePOOL ->
+      encodeListLen 1 <> toCBOR (2 :: Word8)
 
 instance
   (Crypto crypto) =>
   FromCBOR (PredicateFailure (POOL crypto))
   where
   fromCBOR = do
+    n <- decodeListLen
     decodeWord >>= \case
-      0 -> pure StakePoolNotRegisteredOnKeyPOOL
-      1 -> pure StakePoolRetirementWrongEpochPOOL
-      2 -> pure WrongCertificateTypePOOL
+      0 -> do
+        matchSize "StakePoolNotRegisteredOnKeyPOOL" 2 n
+        hk <- fromCBOR
+        pure $ StakePoolNotRegisteredOnKeyPOOL hk
+      1 -> do
+        matchSize "StakePoolRetirementWrongEpochPOOL" 4 n
+        current <- fromCBOR
+        e <- fromCBOR
+        eMax <- fromCBOR
+        pure $ StakePoolRetirementWrongEpochPOOL current e eMax
+      2 -> do
+        matchSize "WrongCertificateTypePOOL" 1 n
+        pure WrongCertificateTypePOOL
       k -> invalidKey k
 
 poolDelegationTransition :: TransitionRule (POOL crypto)
@@ -86,6 +100,7 @@ poolDelegationTransition = do
   case c of
     DCertPool (RegPool poolParam) -> do
       -- note that pattern match is used instead of cwitness, as in the spec
+
       let hk = _poolPubKey poolParam
       if hk ∉ dom stpools
         then -- register new, Pool-Reg
@@ -95,21 +110,39 @@ poolDelegationTransition = do
               { _stPools = StakePools $ stpools ∪ (hk, slot),
                 _pParams = _pParams ps ∪ (hk, poolParam)
               }
-        else -- re-register, Pool-reReg
+        else do
+          -- re-register, Pool-reReg
+          sp <- liftSTS $ asks stabilityWindow
+          firstSlot <- liftSTS $ do
+            ei <- asks epochInfo
+            EpochNo currEpoch <- epochInfoEpoch ei slot
+            epochInfoFirst ei $ EpochNo (currEpoch + 1)
 
-          pure $
-            ps
-              { _pParams = _pParams ps ⨃ (hk, poolParam),
-                _retiring = Set.singleton hk ⋪ _retiring ps
-              }
+          case slot < firstSlot *- Duration sp of
+            True ->
+              pure $ -- non-late re-registration
+                ps
+                  { _pParams = _pParams ps ⨃ (hk, poolParam),
+                    _retiring = Set.singleton hk ⋪ _retiring ps
+                  }
+            False ->
+              pure $ -- late re-registration
+                ps
+                  { _fPParams = _fPParams ps ⨃ (hk, poolParam),
+                    _retiring = Set.singleton hk ⋪ _retiring ps
+                  }
     DCertPool (RetirePool hk (EpochNo e)) -> do
       -- note that pattern match is used instead of cwitness, as in the spec
-      hk ∈ dom stpools ?! StakePoolNotRegisteredOnKeyPOOL
+      hk ∈ dom stpools ?! StakePoolNotRegisteredOnKeyPOOL hk
       EpochNo cepoch <- liftSTS $ do
         ei <- asks epochInfo
         epochInfoEpoch ei slot
       let EpochNo maxEpoch = _eMax pp
-      cepoch < e && e < cepoch + maxEpoch ?! StakePoolRetirementWrongEpochPOOL
+      cepoch < e && e < cepoch + maxEpoch
+        ?! StakePoolRetirementWrongEpochPOOL
+          (EpochNo cepoch)
+          (EpochNo e)
+          (EpochNo $ cepoch + maxEpoch)
       pure $ ps {_retiring = _retiring ps ⨃ (hk, EpochNo e)}
     _ -> do
       failBecause WrongCertificateTypePOOL
